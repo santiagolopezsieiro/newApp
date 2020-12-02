d3a6a13c333c54770781469fae62c7447e113c09
santiago <santiagolopezsieiro@gmail.com>
2020-12-02 15:39:17 -0300

    signIn route added with comparePassword

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -20,4 +20,25 @@ router.post('/signup', async (req, res) => {
     }
 })
 
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body
+
+    if(!email || !password){
+        return res.status(422).send({error: "add password or email"})
+    }
+
+    const user = await User.findOne({email})
+    if(!user){
+        return res.status(422).send({error: "email or password invalid"})
+    }
+
+    try{
+        await user.comparePassword(password)
+        const token = jwt.sign({userId: user._id}, 'MY_SECRET_KEY')
+        res.send({token});
+    } catch(err) {
+        return res.status(422).send({error: "email or password invalid"})
+    }
+})
+
 module.exports = router;
